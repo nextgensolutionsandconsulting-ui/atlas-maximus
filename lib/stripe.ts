4b02f3c445bdb4a324d0a0b8e450975348257590
nextgensolutionsandconsulting-ui <nextgensolutionsandconsulting@gmail.com>
2026-04-15 09:03:34 -0400

    Update stripe.ts

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -3,6 +3,6 @@ import { env } from './env';
 
 export const stripe = env.STRIPE_SECRET_KEY
   ? new Stripe(env.STRIPE_SECRET_KEY, {
-      apiVersion: '2026-03-25.preview',
+      apiVersion: '2025-08-27.basil',
     })
   : null;
