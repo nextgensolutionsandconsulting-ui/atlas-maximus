38a15d3a89170c74fa785b73a76740959a23dac0
nextgensolutionsandconsulting-ui <nextgensolutionsandconsulting@gmail.com>
2026-04-15 15:29:15 -0400

    Update page.tsx

--- app/(protected)/app/onboarding/page.tsx
+++ app/(protected)/app/onboarding/page.tsx
@@ -442,6 +442,7 @@ export default async function OnboardingPage() {
               <Link className="atlas-nav-link" href="/app/account">Account</Link>
               <Link className="atlas-nav-link" href="/app/docs">Docs</Link>
               <Link className="atlas-nav-link" href="/app/support">Support</Link>
+              <Link className="atlas-nav-link" href="/app/get-started">Get Started</Link>  
               </div>
 
               <div className="atlas-nav-actions">
